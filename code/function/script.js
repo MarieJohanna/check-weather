a1b5945823a0cd5ee6f2c348b930e3ff5ab249a6
MarieJohanna <mariejohannanilsson@gmail.com>
2017-11-23 20:23:30 +0100

    .

--- code/function/script.js
+++ code/function/script.js
@@ -10,7 +10,7 @@ const myFunction = () => {
       document.getElementById("cityHeading").innerHTML = json.name
       document.getElementById("celciusCondition").innerHTML = json.main.temp_max + "°C, " + json.weather[0].description
       // Creating the url string for the weather image
-      // const urlWeatherImage = "url(" + "'" + "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png" + "')"
+      // const urlWeatherImage = ('url("http://openweathermap.org/img/w/${json.weather[0].icon}.png")')
       // document.body.style.backgroundImage = urlWeatherImage
       const sunriseToday = new Date(json.sys.sunrise * 1000)
       const sunsetToday = new Date(json.sys.sunset * 1000)
@@ -18,8 +18,8 @@ const myFunction = () => {
       document.getElementById("sunset").innerHTML = "Sunset today at " + sunsetToday.toTimeString()
     }
   )
-  // var offset = new Date().getTimezoneOffset();
 }
-
+// const urlWeatherImage = "url(" + "'" + "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png" + "')"
+// const urlWeatherImage = ('url("http://openweathermap.org/img/w/${json.weather[0].icon}.png")')
 window.onload = myFunction;
 document.getElementById("citySelection").addEventListener("change", myFunction);
