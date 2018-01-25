fd87650bcd7fadf357c7d2ce422ef77667f77e38
MarieJohanna <mariejohannanilsson@gmail.com>
2018-01-25 20:41:12 +0100

    removed unused code

--- code/script.js
+++ code/script.js
@@ -1,24 +1,26 @@
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=a0e04c8ab0e494d9a44f8ea9ec9a17a2").then((response) => {
-  return response.json()
-}).then((json) => {
-  console.log(json)
-  document.getElementById('city').innerHTML = (json.name)
-  document.getElementById('temp').innerHTML = (json.main.temp)
-  document.getElementById('status').innerHTML = (json.weather[0].description)
-  if (json.main.temp < 10) {
-    document.getElementById('weather').classList.toggle('cold')
-  }else if (json.main.temp > 10 && json.main.temp < 20){
-    document.getElementById('weather').classList.toggle('lagom')
-  }else {
-    document.getElementById('weather').classList.toggle('warm')
-  }
-})
+const myFunction = () => {
 
-// fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=a0e04c8ab0e494d9a44f8ea9ec9a17a2").then((response) => {
-//   return response.json()
-// }).then((json) => {
-//   console.log(json)
-//   console.log(json.name)
-//   console.log(json.main.temp)
-//   console.log(json.main.)
-// })
+ const fetchCity = "http://api.openweathermap.org/data/2.5/weather?q=" + document.getElementById("citySelection").value + "&units=metric&APPID=a0e04c8ab0e494d9a44f8ea9ec9a17a2"
+
+ fetch(fetchCity)
+  .then(
+    (response) => {
+      return response.json()
+    }).then((json) => {
+      document.getElementById("cityHeading").innerHTML = json.name
+      document.getElementById("celciusCondition").innerHTML = json.main.temp_max + "°C, " + json.weather[0].description
+      if (json.main.temp_max > 12) {
+        document.getElementById('box').style.background = "orange"
+      }else {
+        document.getElementById('box').style.background = "lightblue"
+      }
+      const sunriseToday = new Date(json.sys.sunrise * 1000)
+      const sunsetToday = new Date(json.sys.sunset * 1000)
+      document.getElementById("sunrise").innerHTML = "Sunrise today at " + sunriseToday.toTimeString()
+      document.getElementById("sunset").innerHTML = "Sunset today at " + sunsetToday.toTimeString()
+    }
+  )
+}
+
+window.onload = myFunction;
+document.getElementById("citySelection").addEventListener("change", myFunction)
