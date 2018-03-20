8ac4da9ea98a487eea64a918e4e77e41631c4fce
MarieJohanna <mariejohannanilsson@gmail.com>
2018-03-20 19:47:52 +0100

    done

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const myFunction = () => {
       document.getElementById("cityHeading").innerHTML = json.name
       document.getElementById("celciusCondition").innerHTML = json.main.temp_max + "°C, " + json.weather[0].description
       if (json.main.temp_max > 12) {
-        document.getElementById('box').style.background = "orange"
+        document.getElementById('box').style.background = "linear-gradient(#f2994a, #f2c94c)"
       }else {
-        document.getElementById('box').style.background = "lightblue"
+        document.getElementById('box').style.background = "linear-gradient(#1c92d2, #f2fcfe)"
       }
-      const sunriseToday = new Date(json.sys.sunrise * 1000)
-      const sunsetToday = new Date(json.sys.sunset * 1000)
-      document.getElementById("sunrise").innerHTML = "Sunrise today at " + sunriseToday.toTimeString()
-      document.getElementById("sunset").innerHTML = "Sunset today at " + sunsetToday.toTimeString()
+      // const sunriseToday = new Date(json.sys.sunrise * 1000)
+      // const sunsetToday = new Date(json.sys.sunset * 1000)
+      // document.getElementById("sunrise").innerHTML = "Sunrise today at " + sunriseToday.toTimeString()
+      // document.getElementById("sunset").innerHTML = "Sunset today at " + sunsetToday.toTimeString()
     }
   )
 }
